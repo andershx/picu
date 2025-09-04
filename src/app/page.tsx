74696a90ebdfb23e2fb04cf186e064234c3a6104
andershx <anderscookiez@gmail.com>
2025-09-04 05:39:05 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import WalletsSection from "@/components/WalletsSection";
 import Footer from "@/components/Footer";
+import TwitterButton from '@/components/TwitterButton';
 
 export default function Home() {
   return (
