85e5a997b9c2394c830432cbd181ce53cc33e63c
andershx <anderscookiez@gmail.com>
2025-09-04 06:24:54 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
-        <WalletsSection />
         <TwitterButton />
+        <WalletsSection />
       </main>
       <Footer />
     </>
